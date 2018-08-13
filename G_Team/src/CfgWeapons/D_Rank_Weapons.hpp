6e6eba41d073e30eba8a57bf7a841ff477f64198
Rav-T <hiddenkrypt@gmail.com>
2018-08-13 14:37:32 -0700

    renamed weapons file and fixed the Gent AWP optics to use the proper CUP slot

---
+++ G_Team/src/CfgWeapons/D_Rank_Weapons.hpp
@@ -0,0 +1,9 @@
+class CUP_srifle_AWM_wdl;
+class Gent_AWP_DMS : CUP_srifle_AWM_wdl {
+  class LinkedItems {
+    class LinkedItemsOptic {
+      item = "optic_DMS";
+      slot = "CUP_PicatinnyTopMountAWM";
+    };
+  };
+};
