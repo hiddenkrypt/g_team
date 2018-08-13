c3bf76be9f3955e81c833dab3d67670a0d337798
Rav-T <hiddenkrypt@gmail.com>
2018-08-13 09:24:00 -0700

    adding faction definition and emblem

---
+++ G_Team/src/CfgFactionClasses.hpp
@@ -0,0 +1,8 @@
+class CfgFactionClasses {
+  class G_Team_Faction {
+    displayName = "GGG Forces";
+    priority = 1; // Position in list. never seems to work for me
+    side = 2; // Opfor = 0, Blufor = 1, Indep = 2.
+    icon = "G_Team\img\ui\logo_CA.paa"; //My icon
+  };
+};
---
+++ G_Team/src/CfgUnitInsignia.hpp
@@ -0,0 +1,8 @@
+class CfgUnitInsignia {
+  class G_Team_Insignia {
+    displayName = "GGG Unit Emblem"; // Name displayed in Arsenal
+    author = "Rav-T"; // Author displayed in Arsenal
+    texture = "G_Team\img\ui\logoOver_CA.paa"; // Image path
+    textureVehicle = ""; // Does nothing currently, reserved for future use
+  };
+};
