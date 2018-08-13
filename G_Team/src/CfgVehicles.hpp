edc875d04db0a4d7943b78c78b6fdd6e7e6799ce
Rav-T <hiddenkrypt@gmail.com>
2018-08-13 09:47:48 -0700

    adding UAZ and Vodnik

--- G_Team/src/CfgVehicles.hpp
+++ G_Team/src/CfgVehicles.hpp
@@ -2,4 +2,5 @@
 //all units are classes defined inside CfgVehicles
 class CfgVehicles{
   #include "CfgVehicles\AirVehicles.hpp" //chopper
+  #include "CfgVehicles\GroundVehicles.hpp" // UAZ command car, Vodnik party van
 };
---
+++ G_Team/src/CfgVehicles/GroundVehicles.hpp
@@ -0,0 +1,17 @@
+class CUP_O_UAZ_Open_CHDKZ;
+class CUP_O_GAZ_Vodnik_PK_RU;
+
+
+//just a UAZ with a different name in editor
+class Gent_Command_Car: CUP_O_UAZ_Open_CHDKZ {
+  GENT_VEHICLE
+  displayName = "Gent Command Car";
+  editorPreview = "\G_Team\img\ui\editor_icons\icon_gent_commandcar_CO.paa"
+};
+
+//just a 2xPKM vodnik with a different editor name. reskins are planned.
+class Gent_Party_Van: CUP_O_GAZ_Vodnik_PK_RU {
+  GENT_VEHICLE
+  displayName = "Gent Party Van";
+  editorPreview = "\G_Team\img\ui\editor_icons\icon_gent_partyvan_CO.paa"
+};
