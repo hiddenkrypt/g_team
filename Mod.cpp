0b8ca8755a7e846d35ff1b50246a42188e552605
Rav-T <hiddenkrypt@gmail.com>
2018-08-13 09:20:05 -0700

    added Mod info file

---
+++ Mod.cpp
@@ -0,0 +1,28 @@
+/// Name of your mod
+name = "G-Team Addon";
+/// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
+picture = "G_Team\img\picture.paa";
+/// Display next to the item added by the mod
+logoSmall = "G_Team\img\logoSmall.paa";
+/// Logo displayed in the main menu
+logo = "G_Team\img\logo.paa";
+/// When the mouse is over, in the main menu
+logoOver = "G_Team\img\logoOver.paa";
+/// Website URL, that can accessed from the expansions menu
+action = "http://bastly.ddns.net/w/index.php/The_G-Team";
+/// Tool tip displayed when the mouse is left over, in the main menu
+tooltipOwned = "GGG Clan Mod";
+/// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] =
+{
+	0.0,
+	0.5,
+	0.25,
+	1
+};
+/// Overview text, displayed from the extension menu
+overview = "Gentlemen's Gaming Group presents the horrid misadventures of the G-Team";
+/// Hide the extension name
+hideName = 0;
+/// Hide the extension menu
+hidePicture = 0;
