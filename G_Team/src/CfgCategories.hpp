6c2f6287a108f1665ae01f6cfe7b1223aee385a1
Rav-T <hiddenkrypt@gmail.com>
2018-08-13 09:51:09 -0700

    adding editor categories for new vehicles

---
+++ G_Team/src/CfgCategories.hpp
@@ -0,0 +1,34 @@
+
+/*
+CfgEditorCategories and CfgEditorSubcategories are used to put units into 3DEN and Zeus's menus
+*/
+
+class CfgEditorCategories {
+	class G_Team {
+		displayName = "G-Team";
+	};
+};
+
+class CfgEditorSubcategories {
+	class GentVehicles {
+		displayName = "Gent Vehicles";
+	};
+	class GentInfantry_S_Rank {
+		displayName = "Gent Infantry (S Rank)";
+	};
+	class GentInfantry_A_Rank {
+		displayName = "Gent Infantry (A Rank)";
+	};
+	class GentInfantry_B_Rank {
+		displayName = "Gent Infantry (B Rank)";
+	};
+	class GentInfantry_C_Rank {
+		displayName = "Gent Infantry (C Rank)";
+	};
+	class GentInfantry_D_Rank {
+		displayName = "Gent Infantry (D Rank)";
+	};
+	class GentUnarmed {
+		displayName = "Unarmed Gents";
+	};
+};
