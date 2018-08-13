14079d729d5e8e5bb015d7fbaf6d5efea9df04f2
Rav-T <hiddenkrypt@gmail.com>
2018-08-13 14:55:39 -0700

    normalized display names

--- G_Team/src/CfgVehicles/D_rank.hpp
+++ G_Team/src/CfgVehicles/D_rank.hpp
@@ -11,7 +11,7 @@ class Gent_AWP_D_Rank: Gent_Soldier {
   scopeCurator = 2;
   editorSubcategory = "GentInfantry_D_Rank"; // editor list category (cfgcategories)
   role = "Gent AWP whore (D Rank)"; //role displayed in MP as a join slot
-  displayName = "Gent AWP whore (D rank)";
+  displayName = "Gent AWP whore (D Rank)";
   //equipment
   backpack = "Gent_AWP_D_Pack";
   weapons[] = {
@@ -72,12 +72,11 @@ class Gent_HUGE_D_Pack : B_AssaultPack_rgr{
     mag_xx(CUP_200Rnd_TE1_Red_Tracer_556x45_M249,3);
   };
 }
-
 class Gent_HUGE_D_Rank: Gent_Soldier {
   scopeCurator = 2;
   editorSubcategory = "GentInfantry_D_Rank"; // editor list category (cfgcategories)
   role = "Gent H.U.G.E. Gunner (D Rank)"; //role displayed in MP as a join slot
-  displayName = "Gent H.U.G.E. (D rank)";
+  displayName = "Gent H.U.G.E. (D Rank)";
   //equipment
   backpack = "Gent_HUGE_D_Pack";
   weapons[] = {
@@ -131,7 +130,6 @@ class Gent_HUGE_D_Rank: Gent_Soldier {
 };
 
 
-
 class CUP_B_GER_Medic_FLecktarn;
 class Gent_medic_D_Pack : CUP_B_GER_Medic_FLecktarn{
   scope = 1;
@@ -146,7 +144,7 @@ class Gent_Medic_D_Rank: Gent_Soldier {
   scopeCurator = 2;
   editorSubcategory = "GentInfantry_D_Rank"; // editor list category (cfgcategories)
   role = "Gent Medic (D Rank)"; //role displayed in MP as a join slot
-  displayName = "Gent Medic (D rank)";
+  displayName = "Gent Medic (D Rank)";
   attendant = 1;
   //equipment
   backpack = "Gent_Medic_D_Pack";
@@ -211,7 +209,7 @@ class Gent_Rifle_D_Rank: Gent_Soldier {
   scopeCurator = 2;
   editorSubcategory = "GentInfantry_D_Rank";
   role = "Gent Rifle (D Rank)";
-  displayName = "Gent Rifle (D rank)";
+  displayName = "Gent Rifle (D Rank)";
   backpack = "Gent_Rifle_D_Pack";
   weapons[] = {
     "CUP_arifle_L85A2",
@@ -262,8 +260,6 @@ class Gent_Rifle_D_Rank: Gent_Soldier {
 };
 
 
-
-
 class B_FieldPack_khk;
 class Gent_Mule_D_Pack : B_FieldPack_khk{
   scope = 1;
@@ -279,7 +275,7 @@ class Gent_Mule_D_Rank: Gent_Soldier {
   scopeCurator = 2;
   editorSubcategory = "GentInfantry_D_Rank"; // editor list category (cfgcategories)
   role = "Gent Mule (D Rank)"; //role displayed in MP as a join slot
-  displayName = "Gent Mule (D rank)";
+  displayName = "Gent Mule (D Rank)";
   //equipment
   backpack = "Gent_Mule_D_Pack";
   weapons[] = {
@@ -329,8 +325,6 @@ class Gent_Mule_D_Rank: Gent_Soldier {
 };
 
 
-
-
 class Gent_Boom_D_Pack : B_FieldPack_khk{
   scope = 1;
   class TransportMagazines {
@@ -346,7 +340,7 @@ class Gent_Boom_D_Rank: Gent_Soldier {
   scopeCurator = 2;
   editorSubcategory = "GentInfantry_D_Rank";
   role = "Gent Explosions! (D Rank)";
-  displayName = "Gent Explosions! (D rank)";
+  displayName = "Gent Explosions! (D Rank)";
   //equipment
   backpack = "Gent_Boom_D_Pack";
   weapons[] = {
