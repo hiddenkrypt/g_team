8d85db737401be44a95eba2beebdad92213a7619
Rav-T <hiddenkrypt@gmail.com>
2018-08-21 11:39:06 -0700

    added goodluck sound file and cfgsounds reference for mission 00

--- G_Team/src/CfgSounds.hpp
+++ G_Team/src/CfgSounds.hpp
@@ -4,21 +4,26 @@ class CfgSounds
 	class G_Team_airhorn{
 		name = "airhorn";   // how the sound is referred to in the editor (e.g. trigger effects)
 		sound[] = {"G_Team\sfx\airhorn.ogg", 1, 1, 400}; // filename, volume, pitch, distance
-		titles[] = {1, "*DOOT.*"};  // subtitle delay in seconds, subtitle text
+		titles[] = {0, "*DOOT.*"};  // subtitle delay in seconds, subtitle text
 	};
 	class G_Team_airporn{
 		name = "airporn";
 		sound[] = {"G_Team\sfx\airporn.ogg", 1, 1, 400};
-		titles[] = {1, "*DOOT DOODOODOODOOT*"};
+		titles[] = {0, "*DOOT DOODOODOODOOT*"};
 	};
   class G_Team_hello{
     name = "hello";
     sound[] = {"G_Team\sfx\hello.ogg", 1, 1, 100};
-    titles[] = {1, "HELLO"};
+    titles[] = {0, "HELLO"};
   };
   class G_Team_alertVA{
 		name = "alertVA";
 		sound[] = {"G_Team\sfx\alertVA.ogg", 1, 1, 300};
-		titles[] = {2, "'All personnel: report to the main courtyard for emergency briefing. I say again. All personnel: report to the main courtyard for briefing.'"};
+		titles[] = {1, "'All personnel: report to the main courtyard for emergency briefing. I say again. All personnel: report to the main courtyard for briefing.'"};
+  };
+  class G_Team_goodLuck{
+		name = "alertVA";
+		sound[] = {"G_Team\sfx\goodLuck.ogg", 1, 1, 400};
+		titles[] = {0, "'Good Luck!'"};
   };
 };
