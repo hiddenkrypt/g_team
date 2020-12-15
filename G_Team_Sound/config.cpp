96d97a78b9dba6ac6c8261e6b2f8217bf1248d0b
ada <hiddenkrypt@gmail.com>
2020-12-15 10:56:22 -0800

    fixed depricated author name, fixed formatting

--- G_Team_Sound/config.cpp
+++ G_Team_Sound/config.cpp
@@ -1,15 +1,12 @@
-class CfgPatches
-{
-  class GGG_Team_Mod_Sounds
-  {
-    name = "G-Team Addon";
-    author = "Rav-T";
-    units[] = {
-    };
-    requiredVersion = 0.1;
-    requiredAddons[] = {
-    };
-  };
+class CfgPatches {
+	class GGG_Team_Mod_Sounds {
+		name = "G-Team Addon";
+		author = "Ada";
+		requiredVersion = 0.1;
+		requiredAddons[] = {
+			"GGG_Team_Mod"
+		};
+	};
 };
 
 #include "src\CfgSounds.hpp"
