e4fd85036f309d340241469230dbab3283819011
ada <hiddenkrypt@gmail.com>
2020-12-15 10:52:20 -0800

    cleaned up mod.cpp comments, fixed website url

--- Mod.cpp
+++ Mod.cpp
@@ -1,22 +1,12 @@
-/// Name of your mod
+
 name = "G-Team Addon";
-/// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
-picture = "G_Team\img\picture.paa";
-/// Display next to the item added by the mod
-logoSmall = "G_Team\img\ui\logoSmall_CA.paa";
-/// Logo displayed in the main menu
+picture = "G_Team\img\picture.paa" 
+logoSmall = "G_Team\img\ui\logoSmall_CA.paa"; 
 logo = "G_Team\img\ui\logo_CA.paa";
-/// When the mouse is over, in the main menu
 logoOver = "G_Team\img\ui\logoOver_CA.paa";
-/// Website URL, that can accessed from the expansions menu
-action = "http://bastly.ddns.net/w/index.php/The_G-Team";
-/// Tool tip displayed when the mouse is left over, in the main menu
+action = "http://bast.ddns.net/w/index.php/The_G-Team";
 tooltipOwned = "GGG Clan Mod";
-/// Color used for DLC stripes and backgrounds (RGBA)
 dlcColor[] = { 0.0, 0.5, 0.25, 1 };
-/// Overview text, displayed from the extension menu
 overview = "Gentlemen's Gaming Group presents the horrid misadventures of the G-Team";
-/// Hide the extension name
 hideName = 0;
-/// Hide the extension menu
 hidePicture = 0;
